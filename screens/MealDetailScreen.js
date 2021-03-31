1d0d803acb2e5a1970baed981958dc4d04679756
Alex <7894664@mail.ru>
2021-03-31 14:18:14 +0300

    Add navigation with between components(navigate, pop, goBack)

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet, Button} from "react-native";
 
 const MealDetailScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Meal Detail Screen</Text>
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <Text>The Meal Detail Screen</Text>
+            <Button title={'Go Back'} onPress={() => {
+                props.navigation.pop();
+            }}/>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
